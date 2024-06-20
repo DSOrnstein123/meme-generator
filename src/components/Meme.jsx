75a78c2df1a4adbf8564ff5c2484c82a0a8cabb2
DSOrnstein <namphonggithub@gmail.com>
2024-06-20 15:47:05 +0700

    Make "get random meme" button interactive

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,32 @@
+import memesData from "../assets/memes-data.js";
+import React from "react";
+
 function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImg: "https://i.imgflip.com/30b1gx.jpg",
+  });
+
+  const [allMemeImgs, setAllMemeImgs] = React.useState(memesData);
+
+  function getMemeImg() {
+    const memesArray = allMemeImgs.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const { url } = memesArray[randomNumber];
+    setMeme((prevMeme) => ({ ...prevMeme, randomImg: url }));
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input className="form-input" type="text" placeholder="Top text" />
         <input className="form-input" type="text" placeholder="Bottom text" />
-        <button className="form-button">Get a new meme image 🖼</button>
-      </form>
+        <button onClick={getMemeImg} className="form-button">
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={meme.randomImg} className="meme-img" />
     </main>
   );
 }
